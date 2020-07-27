8c865c72a001486e14cf0c0a4d2a5ff7b1f34f96
the-last-martian <the-last-martian@martian.localdomain>
2020-07-27 23:53:53 +0200

    Verified by Cia v2.1.1

--- script.js
+++ script.js
@@ -1,52 +1,67 @@
-
 const probiere = document.querySelector('.probiere');
 const ausgabe = document.querySelector('.ausgabe');
 const hack = document.querySelector('.hack');
 
 const fetchJSON = async (query, body) => {
-  let response = await fetch(
-    'http://localhost:9922' + query, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(body)
-  });
-  return await response.json();
+    let response = await fetch(
+        'http://localhost:9922' + query, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        });
+    return await response.json();
 };
 
 hack.onclick = async () => {
-  let passwortHack = '';
+    let passwortHack = '';
 
-  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXZYabcdefghijklmnopqrstuvwxzy01234567890!@_-'
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXZYabcdefghijklmnopqrstuvwxzy01234567890!@_-'
 
     for (let a = 0; a < alphabet.length; a++) {
 
-      zeichenA = alphabet[a];
+        zeichenA = alphabet[a];
 
-      for (let b = 0; b < alphabet.length; b++) {
+        for (let b = 0; b < alphabet.length; b++) {
 
-        zeichenB = alphabet[b]
+            zeichenB = alphabet[b]
 
-        for (let c = 0; c < alphabet.length; c++){
+            for (let c = 0; c < alphabet.length; c++) {
 
-          zeichenC = alphabet[c]
+                zeichenC = alphabet[c]
 
-          passwortHack = zeichenA + zeichenB + zeichenC
+                for (let d = 0; d < alphabet.length; d++) {
+
+                    zeichenD = alphabet[d]
+
+                    for (let e = 0; e < alphabet.length; e++) {
+
+                        zeichenE = alphabet[e]
+
+                        for (let f = 0; f < alphabet.length; f++) {
+
+                            zeichenF = alphabet[f]
+                            passwortHack = zeichenA + zeichenB + zeichenC + zeichenD + zeichenE + zeichenF
 
 
-          let data = await fetchJSON(
-            '/login', { pass: passwortHack });
+                            let data = await fetchJSON(
+                                '/login', {
+                                    pass: passwortHack
+                                });
 
-          if (data.status === 'success') {
-            alert('Hacked')
-            return
-          } else {
-            console.log(passwortHack)
-           // document.write(passwortHack)
-            document.getElementById("demo").innerText = passwortHack
-          }
+                            if (data.status === 'success') {
+                                alert('Hacked')
+                                return
+                            } else {
+                                console.log(passwortHack)
+                                // document.write(passwortHack)
+                                document.getElementById("demo").innerText = passwortHack
+                            }
+                        }
+                    }
+                }
+            }
         }
-
-      }
     }
-  }
-  
+}
--- server.js
+++ server.js
@@ -8,23 +8,29 @@ a.use(p.json());
 
 
 let passwort = '';
-for ( let i = 0; i < 3; i++ ){
-  let zeichen;
-  do {
-    zeichen = String.fromCharCode( Math.round(Math.random()*255) )
-  } while ( ! zeichen.match(/[-_@a-z0-9]/i) )
-  passwort += zeichen;
+for (let i = 0; i < 6; i++) {
+    let zeichen;
+    do {
+        zeichen = String.fromCharCode(Math.round(Math.random() * 255))
+    } while (!zeichen.match(/[-_@a-z0-9]/i))
+    passwort += zeichen;
 }
 console.log('passwort: ' + passwort);
 
-a.post("/login",(req,res)=>{
-  let { pass } = req.body;
-  if ( pass === passwort ){
-    res.json({status:'success'});
-  } else {
-    res.json({status:'fail'});
-  }
+a.post("/login", (req, res) => {
+    let {
+        pass
+    } = req.body;
+    if (pass === passwort) {
+        res.json({
+            status: 'success'
+        });
+    } else {
+        res.json({
+            status: 'fail'
+        });
+    }
 });
 
 
-a.listen(9922,e=>console.log('server bereit auf port '+9922));
+a.listen(9922, e => console.log('server bereit auf port ' + 9922));
